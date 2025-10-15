d45981ef065241f70e447eb1bb0fab781e841aca
esspooki <esspooki@gmail.com>
2025-10-15 03:36:55 +0000

    Busqueda parcial de maestro agregada

--- query_basico.py
+++ query_basico.py
@@ -10,11 +10,22 @@ def buscar_materia(materia):
 def buscar_nrc(nrc):
     return next((c for c in classes if c['NRC'] == nrc), None)
 
+def buscar_maestro(maestro):
+    return [c for c in classes if maestro.lower() in c['Maestro'].lower()]
+
 # Ejemplo:
 if __name__ == "__main__":
+    print("\n Resultado por materia:\n")
+
     result = buscar_materia("PROGRAMACION ESTRUCTURADA")
     for r in result:
         print(r)
 
+    print("\n Resultado por NRC:\n")
+
     nrc_resultado = buscar_nrc("193703")
     print(nrc_resultado)
+
+    print("\n Resultado por maestro (busqueda parcial):\n")
+    maestro_resultado = buscar_maestro("JORGE ERNESTO")
+    print(maestro_resultado)
